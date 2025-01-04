a3361e4fdb0d70ce85ceb43bb9e01d97702ae167
Jason Livesay <runvnc@gmail.com>
2025-01-04 06:55:35 -0600

    update

--- src/mr_hud/router.py
+++ src/mr_hud/router.py
@@ -1,10 +1,14 @@
 from lib.chatcontext import ChatContext
 from fastapi import APIRouter, Request 
-
+from lib.providers.services import service_manager
+from lib.providers.commands import command_manager
+i
 router = APIRouter()
 
 @router.get("/session_data/{log_id}")
 async def session_data(request: Request, log_id: str):
-    context = ChatContext(log_id)
+    user = request.state.user.username
+    context = ChatContext(command_manager, service_manager, user)
+    await context.load_context(log_id)
     sess_data = context.data["session"]
     return sess_data
